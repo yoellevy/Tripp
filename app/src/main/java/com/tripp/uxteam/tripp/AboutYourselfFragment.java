2a86c874d3719ef168a0f84d61803ff7131729b0
yoellevy <yoel.levy@mail.huji.ac.il>
2018-11-30 21:51:56 +0200

    adding change fragment capability

--- app/src/main/java/com/tripp/uxteam/tripp/AboutYourselfFragment.java
+++ app/src/main/java/com/tripp/uxteam/tripp/AboutYourselfFragment.java
@@ -1,28 +1,23 @@
 package com.tripp.uxteam.tripp;
 
-import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SignScreen.OnFragmentInteractionListener} interface
+ * {@link AboutYourselfFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SignScreen#newInstance} factory method to
+ * Use the {@link AboutYourselfFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SignScreen extends Fragment {
+public class AboutYourselfFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -34,7 +29,7 @@ public class SignScreen extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public SignScreen() {
+    public AboutYourselfFragment() {
         // Required empty public constructor
     }
 
@@ -44,11 +39,11 @@ public class SignScreen extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SignScreen.
+     * @return A new instance of fragment AboutYourselfFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SignScreen newInstance(String param1, String param2) {
-        SignScreen fragment = new SignScreen();
+    public static AboutYourselfFragment newInstance(String param1, String param2) {
+        AboutYourselfFragment fragment = new AboutYourselfFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,29 +61,17 @@ public class SignScreen extends Fragment {
     }
 
     @Override
-    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_yourself, container, false);
+    }
 
-        View view = inflater.inflate(R.layout.fragment_sign_screen, container, false);
-        Button button = (Button) view.findViewById(R.id.signup_btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Toast toast = Toast.makeText(inflater.getContext(),
-//                        "sign up", Toast.LENGTH_SHORT);
-//
-//                toast.show();
-                AboutYourselfFragment fragment = AboutYourselfFragment.newInstance("a", "b");
-                FragmentManager fragmentManager = MainActivity.GetInstance().fragmentManager;
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, fragment);
-                fragmentTransaction.commit();
-            }
-        });
-        return view;
-
-//        return inflater.inflate(R.layout.fragment_sign_screen, container, false);
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
     }
 
     @Override
@@ -106,14 +89,6 @@ public class SignScreen extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    public void signupOnClick(View view) {
-        AboutYourselfFragment fragment = AboutYourselfFragment.newInstance("a", "b");
-        FragmentManager fragmentManager = MainActivity.GetInstance().fragmentManager;
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
-        fragmentTransaction.commit();
     }
 
     /**
--- app/src/main/java/com/tripp/uxteam/tripp/MainActivity.java
+++ app/src/main/java/com/tripp/uxteam/tripp/MainActivity.java
@@ -1,11 +1,23 @@
 package com.tripp.uxteam.tripp;
 
+
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SignScreen.OnFragmentInteractionListener, AboutYourselfFragment.OnFragmentInteractionListener {
+
+    private static MainActivity instance = null;
+
+    public static MainActivity GetInstance() {
+        return instance;
+    }
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.fragment_sign_screen);
+
+        SignScreen signScreenFragment = SignScreen.newInstance("a", "b");
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, signScreenFragment);
+        fragmentTransaction.commit();
+
+//        setContentView(R.layout.fragment_sign_screen);
+        setContentView(R.layout.activity_main);
+        instance = this;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
