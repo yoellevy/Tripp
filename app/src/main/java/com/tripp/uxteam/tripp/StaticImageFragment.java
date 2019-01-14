dabcd3f2f247cf627f25cf9faa87f6ad3e5d5b86
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2019-01-14 13:17:41 +0200

    added a fragment between sign in and about fragment

--- app/src/main/java/com/tripp/uxteam/tripp/StaticImageFragment.java
+++ app/src/main/java/com/tripp/uxteam/tripp/StaticImageFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -23,12 +24,13 @@ import java.util.TimerTask;
 public class StaticImageFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String IMG_TO_LOAD = "param1";
+    private static final String FRAGMENT_TO_PASS_TO = "param2";
+    enum FRAGMENTS {ABOUT_YOURSELF}
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String ImgToLoad;
+    private String FragmentToPassTo;
 
 //    private OnFragmentInteractionListener mListener;
 
@@ -40,16 +42,16 @@ public class StaticImageFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param imgToLoad Parameter 1.
+     * @param fragmentToPassTo Parameter 2.
      * @return A new instance of fragment StaticImageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static StaticImageFragment newInstance(String param1, String param2) {
+    public static StaticImageFragment newInstance(String imgToLoad, String fragmentToPassTo) {
         StaticImageFragment fragment = new StaticImageFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(IMG_TO_LOAD, imgToLoad);
+        args.putString(FRAGMENT_TO_PASS_TO, fragmentToPassTo);
         fragment.setArguments(args);
         return fragment;
     }
@@ -58,29 +60,40 @@ public class StaticImageFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            ImgToLoad = getArguments().getString(IMG_TO_LOAD);
+            FragmentToPassTo = getArguments().getString(FRAGMENT_TO_PASS_TO);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_static_image, container, false);
+        ImageView backgroundImg = view.findViewById(R.id.static_img_background_img);
+
+        backgroundImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToAboutFragment();
+            }
+        });
+
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 changeToAboutFragment();
             }
         }, 1500);
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_static_image, container, false);
+
+        return view;
     }
 
     private void changeToAboutFragment(){
         AboutYourselfFragment fragment = AboutYourselfFragment.newInstance();
         FragmentManager fragmentManager = MainActivity.GetInstance().fragmentManager;
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, fragment, "ABOUT_FRAGMENT");
+        fragmentTransaction.add(R.id.fragment_container, fragment, null);
         fragmentTransaction.commit();
     }
 
